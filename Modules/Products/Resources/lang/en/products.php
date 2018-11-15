cb6e098a58becc83badc36d9b92eac803d906304
laravelbap <32711573+laravelbap@users.noreply.github.com>
2018-11-15 16:10:44 +0100

    English language

---
+++ Modules/Products/Resources/lang/en/products.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+
+    'module' => 'Products & Services',
+    'module_description' => 'All kind of goods or services that your company offers.',
+    'delete' => 'Delete',
+    'edit' => 'Edit',
+    'create' => 'Create',
+    'create_new' => 'Create Product or Service',
+    'back' => 'Back',
+    'details' => 'Details',
+    'list' => 'Products & Services list',
+    'updated' => 'Entry updated',
+    'created' => 'Entry created',
+
+    'dict' => [
+            'product' => 'Product',
+            'service' => 'Service',
+            'hardware' => 'Hardware',
+            'software' => 'Software',
+            'other' => 'Other'
+    ],
+
+    'panel' => [
+        'information' => 'Basic Information',
+        'notes' => 'Notes'
+    ],
+
+    'form' => [
+        'notes' => 'Notes',
+        'name' => 'Name',
+        'part_number' => 'Part number',
+        'vendor_part_number' => 'Vendor part number',
+        'website' => 'Website',
+        'price' => 'Price',
+        'vat' => 'Vat',
+        'owned_by' => 'Assigned To',
+        'vendor_id' => 'Vendor',
+        'product_type_id' => 'Type',
+        'product_category_id' => 'Category',
+        'serial_no' => 'Serial NO',
+        'product_sheet' => 'Product Sheet',
+        'image_path' => 'Product Image',
+        'category' => 'Category',
+        'type' => 'Type',
+        'vendor_name' => 'Vendor name'
+    ],
+
+    'table' => [
+        'image_path' => 'Product Image'
+    ],
+
+    'settings' => [
+        'type' => 'Type',
+        'category' => 'Category'
+    ],
+
+    'type' => [
+        'module' => 'Type',
+        'module_description' => 'Manage type',
+        'panel' => [
+            'details' => 'Details'
+        ],
+        'form' => [
+            'name' => 'Name'
+        ],
+    ],
+
+
+    'category' => [
+        'module' => 'Category',
+        'module_description' => 'Manage category',
+        'panel' => [
+            'details' => 'Details'
+        ],
+        'form' => [
+            'name' => 'Name'
+        ],
+    ],
+
+
+
+];
